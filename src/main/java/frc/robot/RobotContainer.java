866423e4bd09ac815fdce9a9a6ee32eb380b0850
rohannella <roniereddy@gmail.com>
2024-11-26 15:41:07 -0600

    hello!

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,16 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class RobotContainer {
+  public RobotContainer() {
+    configureBindings();
+  }
+
+  private void configureBindings() {
+  }
+
+  public Command getAutonomousCommand() {
+    return null;
+  }
+}
