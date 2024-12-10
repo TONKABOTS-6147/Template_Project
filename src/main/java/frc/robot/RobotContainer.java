f4a6322c57e5f713dcb2c928e9776ca1b9b205c4
neverwascreative <rngu2029@277apps.org>
2024-12-10 15:50:39 -0600

    made commands for the joystick

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,13 +1,23 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.commands.TransferMotorUpCommand;
+import frc.robot.subsystems.TransferSubsystem;
 
 public class RobotContainer {
+
+  TransferSubsystem transferSubsystem = new TransferSubsystem();
+  Joystick driverJoystick = new Joystick(0);
+  JoystickButton buttonA = new JoystickButton(driverJoystick, 1);
+
   public RobotContainer() {
     configureBindings();
   }
 
   private void configureBindings() {
+    buttonA.whileTrue(new TransferMotorUpCommand(transferSubsystem));
   }
 
   public Command getAutonomousCommand() {
